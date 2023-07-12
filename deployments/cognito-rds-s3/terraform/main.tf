bd69f3090b4b142838c7d7102711aa566aca8aa2
Mohammed-5253 <mohammed22091995@gmail.com>
2023-07-12 18:26:40 +0530

    provider disabled for now

--- deployments/cognito-rds-s3/terraform/main.tf
+++ deployments/cognito-rds-s3/terraform/main.tf
@@ -66,30 +66,30 @@ provider "aws" {
   secret_key = var.aws_terraform_user_access_secret_key
 }
 
-provider "kubernetes" {
-  host                   = module.eks_blueprints.eks_cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.ai-boat.token
-
-  /*
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", module.eks_blueprints.eks_cluster_id]
-  }
-  */
-}
-
-data "aws_eks_cluster_auth" "ai-boat" {
-  name = var.cluster_name
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = module.eks_blueprints.eks_cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
-    token                  = data.aws_eks_cluster_auth.ai-boat.token
+#provider "kubernetes" {
+#  host                   = module.eks_blueprints.eks_cluster_endpoint
+#  cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
+#  token                  = data.aws_eks_cluster_auth.ai-boat.token
+#
+#  /*
+#  exec {
+#    api_version = "client.authentication.k8s.io/v1beta1"
+#    command     = "aws"
+#    # This requires the awscli to be installed locally where Terraform is executed
+#    args = ["eks", "get-token", "--cluster-name", module.eks_blueprints.eks_cluster_id]
+#  }
+#  */
+#}
+#
+#data "aws_eks_cluster_auth" "ai-boat" {
+#  name = var.cluster_name
+#}
+#
+#provider "helm" {
+#  kubernetes {
+#    host                   = module.eks_blueprints.eks_cluster_endpoint
+#    cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
+#    token                  = data.aws_eks_cluster_auth.ai-boat.token
 
     /*
     exec {
@@ -99,8 +99,8 @@ provider "helm" {
       args = ["eks", "get-token", "--cluster-name", module.eks_blueprints.eks_cluster_id]
     }
     */
-  }
-}
+#  }
+#}
 
 data "aws_ec2_instance_type_offerings" "availability_zones_cpu" {
   filter {
