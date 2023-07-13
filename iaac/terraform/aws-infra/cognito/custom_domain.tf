6b9fcf9f3f700f53f711b38df04f525baff15725
Mohammed-5253 <mohammed22091995@gmail.com>
2023-07-13 20:28:21 +0530

    custom domain added again.

--- iaac/terraform/aws-infra/cognito/custom_domain.tf
+++ iaac/terraform/aws-infra/cognito/custom_domain.tf
@@ -61,27 +61,27 @@ resource "aws_acm_certificate_validation" "cognito_domain" {
   validation_record_fqdns = [for record in aws_route53_record.certificate_validation_cognito_domain : record.fqdn]
 }
 
-#resource "aws_cognito_user_pool_domain" "platform" {
-#  domain          = "auth.${data.aws_route53_zone.platform.name}"
-#  certificate_arn = aws_acm_certificate.cognito_domain_cert.arn
-#  user_pool_id    = aws_cognito_user_pool.platform.id
-#
-#  depends_on = [
-#    aws_route53_record.pre_cognito_domain_a_record, aws_acm_certificate.cognito_domain_cert, aws_acm_certificate_validation.cognito_domain
-#  ]
-#}
-#
-#resource "aws_route53_record" "auth_cognito_domain_record" {
-#  allow_overwrite = true
-#  name            = aws_cognito_user_pool_domain.platform.domain
-#  type            = "A"
-#  zone_id         = data.aws_route53_zone.platform.zone_id
-#  alias {
-#    evaluate_target_health = false
-#    name                   = aws_cognito_user_pool_domain.platform.cloudfront_distribution_arn
-#    # For creating an alias record to other AWS resource, route53 needs hosted zone id and DNS name.
-#    # Since CloudFront is a global service, there is only one hosted zone id
-#    # https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-route53-aliastarget.html
-#    zone_id = "Z2FDTNDATAQYW2"
-#  }
-#}
+resource "aws_cognito_user_pool_domain" "platform" {
+  domain          = "auth.${data.aws_route53_zone.platform.name}"
+  certificate_arn = aws_acm_certificate.cognito_domain_cert.arn
+  user_pool_id    = aws_cognito_user_pool.platform.id
+
+  depends_on = [
+    aws_route53_record.pre_cognito_domain_a_record, aws_acm_certificate.cognito_domain_cert, aws_acm_certificate_validation.cognito_domain
+  ]
+}
+
+resource "aws_route53_record" "auth_cognito_domain_record" {
+  allow_overwrite = true
+  name            = aws_cognito_user_pool_domain.platform.domain
+  type            = "A"
+  zone_id         = data.aws_route53_zone.platform.zone_id
+  alias {
+    evaluate_target_health = false
+    name                   = aws_cognito_user_pool_domain.platform.cloudfront_distribution_arn
+    # For creating an alias record to other AWS resource, route53 needs hosted zone id and DNS name.
+    # Since CloudFront is a global service, there is only one hosted zone id
+    # https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-route53-aliastarget.html
+    zone_id = "Z2FDTNDATAQYW2"
+  }
+}
