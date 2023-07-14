c879b522a64d9ffe489ca89097075ff43995aa8d
Mohammed-5253 <mohammed22091995@gmail.com>
2023-07-14 10:08:58 +0530

    helm addon source as it is.

--- iaac/terraform/common/aws-secrets-manager/main.tf
+++ iaac/terraform/common/aws-secrets-manager/main.tf
@@ -1,5 +1,5 @@
 module "helm_addon" {
-  source = "github.com/codinglama-tech/terraform-aws-eks-blueprints//modules/kubernetes-addons/helm-addon?ref=fix-aws-auth-dep-nodegroup"
+  source        = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons/helm-addon?ref=v4.31.0"
   helm_config   = local.helm_config
   addon_context = var.addon_context
 }
