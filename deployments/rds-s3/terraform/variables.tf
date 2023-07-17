ae95fae8a22b367c60421a11d26bc029212d49a6
Mohammed-5253 <mohammed22091995@gmail.com>
2023-07-17 18:47:04 +0530

    error1

--- deployments/rds-s3/terraform/variables.tf
+++ deployments/rds-s3/terraform/variables.tf
@@ -205,30 +205,30 @@ variable "notebook_idleness_check_period" {
   type        = string
   default     = 5
 }
-
-variable "nlb_config" {
-  type = object({
-    name        = string
-    internal    = string
-    environment = string
-    subnet      = string
-    nlb_vpc_id  = string
-  })
-  description = "Configuration for the network load balancer."
-}
-
-variable "forwarding_config" {
-  type        = map(string)
-  description = "Port forwarding configuration for the network load balancer."
-}
-
-variable "tg_config" {
-  type = object({
-    name                  = string
-    target_type           = string
-    health_check_protocol = string
-    tg_vpc_id             = string
-    target_id1            = string
-  })
-  description = "Configuration for the target group."
-}
+#
+#variable "nlb_config" {
+#  type = object({
+#    name        = string
+#    internal    = string
+#    environment = string
+#    subnet      = string
+#    nlb_vpc_id  = string
+#  })
+#  description = "Configuration for the network load balancer."
+#}
+#
+#variable "forwarding_config" {
+#  type        = map(string)
+#  description = "Port forwarding configuration for the network load balancer."
+#}
+#
+#variable "tg_config" {
+#  type = object({
+#    name                  = string
+#    target_type           = string
+#    health_check_protocol = string
+#    tg_vpc_id             = string
+#    target_id1            = string
+#  })
+#  description = "Configuration for the target group."
+#}
